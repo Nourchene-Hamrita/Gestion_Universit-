873393451fab7b9f441ce37543ae11071d7b3b72
Fares Manai <fares.manaisademx@gmail.com>
2023-01-22 18:31:12 +0000

    crudOptions default

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,6 +2,8 @@ const db = {};
 db.User = require('./user.model');
 db.Admin = require('./admin.model');
 for (const key in db) {
+  if (!db[key].crudOptions)
+    db[key].crudOptions = {}
   db[key].Ndb = db
 }
 module.exports = db;
